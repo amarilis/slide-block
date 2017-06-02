3b7c44e5482049dfbb1e65424083083eeda852f2
Mountain Programmer <amarilis-2005@yandex.ru>
2017-06-02 12:56:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+    document.querySelector('.left').style.left = 0;
+}, 1000);
+
+setTimeout(function () {
+    document.querySelector('.right').style.right = 0;
+}, 1500);
---
+++ readme.md
@@ -0,0 +1,16 @@
+index.html
+- html
+- style.css
+- main.js
+
+---
+
+function-animate.html
+- react component
+- style.css
+
+---
+
+react-animate.html
+- react component
+- style.css
